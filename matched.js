6f0359092449eeae4fdffa1c8769c076867b4c2d
mdshakil04 <m.shakil04@yahoo.com>
2024-02-13 22:48:29 +0600

    Simplified version of for loop

---
+++ matched.js
@@ -0,0 +1,8 @@
+const numbers = [12, 34, 32, 56, 766, 23]
+// for(let i = 0; i < numbers.length; i++){
+//     const number = numbers[i]
+//     console.log(number);
+// }
+for(const number of numbers){
+    console.log(number)
+}
---
+++ object.js
@@ -0,0 +1,12 @@
+const student = {
+    name: 'Mehrima',
+    id: 1,
+    address: 'Katakhali',
+    friends: ['Sharina', 'Tanisha', 'Tahiya', 'Tabassun', 'Merroz'],
+    isJinious: true,
+    toys:{
+        brand: 'Sindrella',
+        price: 2500
+    }
+}
+console.log(student)
