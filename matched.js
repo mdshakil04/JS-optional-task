53c608e140c8ea0a1f1be95636413e5b673b6480
mdshakil04 <m.shakil04@yahoo.com>
2024-02-13 23:35:05 +0600

    Product matched by string function

--- matched.js
+++ matched.js
@@ -4,5 +4,27 @@ const numbers = [12, 34, 32, 56, 766, 23]
 //     console.log(number);
 // }
 for(const number of numbers){
-    console.log(number)
+    // console.log(number)
 }
+const products = [
+    {id:1, name: "Samsung Phone", price: 12356},
+    {id:2, name: "Walton Laptop", price: 12656},
+    {id:3, name: "Realme Phone", price: 42356},
+    {id:4, name: "Tecno phone", price: 2356},
+    {id:5, name: "Dell laptop", price: 52356},
+    {id:6, name: "Acer laptop", price: 32356},
+    {id:7, name: "Macbook Pro laptop Phone", price: 232356},
+    {id:8, name: "Xiaomi Phone", price: 12356},
+    {id:9, name: "Samsung laptop", price: 92356}
+]
+function matchedProduct(products,search){
+    const matched = [];
+    for(const product of products){
+        if(product.name.toLowerCase().includes(search.toLowerCase())){
+            matched.push(product)
+        }
+    }
+    return matched;
+}
+const myProduct = matchedProduct(products, 'laptop')
+console.log(myProduct)
